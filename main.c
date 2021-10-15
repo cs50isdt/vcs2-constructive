e605f1b2ab43b20cc1959b38b8da0b839feea24c
Stuart S. Scolastico <stuart.scolastico@tufts.edu>
2021-10-15 02:35:09 -0400

    Make main a simple calculator

--- main.c
+++ main.c
@@ -1,6 +1,49 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  printf("Hello, world!\n");
+int calc(int left, char op, int right) {
+  switch (op) {
+  case '+':
+    return left + right;
+  case '-':
+    return left - right;
+  case '*':
+    return left * right;
+  case '/':
+    return left / right;
+  }
+  fprintf(stderr, "Unrecognized op `%c'.\n", op);
+  exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    fprintf(stderr,
+            "Usage: %s <num> <op> <num>\nWhere <op> is one of +, -, *, /.\n",
+            argv[0]);
+    return EXIT_FAILURE;
+  }
+  const char *left_str = argv[1];
+  const char *op_str = argv[2];
+  const char *right_str = argv[3];
+  errno = 0;
+  int left = strtol(left_str, NULL, 10);
+  if (errno != 0) {
+    perror(argv[0]);
+    return EXIT_FAILURE;
+  }
+  if (op_str[1] != '\0') {
+    fprintf(stderr, "Op must be one character. Got `%s'.\n", op_str);
+    return EXIT_FAILURE;
+  }
+  errno = 0;
+  int right = strtol(right_str, NULL, 10);
+  if (errno != 0) {
+    perror(argv[0]);
+    return EXIT_FAILURE;
+  }
+  int result = calc(left, op_str[0], right);
+  fprintf(stdout, "%d\n", result);
   return 0;
 }
