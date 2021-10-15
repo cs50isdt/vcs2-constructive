2157596701c62d36c68c7b77dff733c2f3b3fe8a
Dr. Garbarini <dottor@garbarini.net>
2021-10-15 10:46:33 -0400

    Add helper to_int function
    Can't for the life of me remember how to use errno

--- main.c
+++ main.c
@@ -17,6 +17,20 @@ int calc(int left, char op, int right) {
   exit(EXIT_FAILURE);
 }
 
+int to_int(const char *str, int *resultp) {
+  char *endptr;
+  errno = 0;
+  int result = strtol(str, &endptr, 10);
+  if (str == endptr) {
+    errno = EINVAL;
+  }
+  if (errno != 0) {
+    return -1;
+  }
+  *resultp = result;
+  return 0;
+}
+
 int main(int argc, char **argv) {
   if (argc != 4) {
     fprintf(stderr,
@@ -27,13 +41,8 @@ int main(int argc, char **argv) {
   const char *left_str = argv[1];
   const char *op_str = argv[2];
   const char *right_str = argv[3];
-  errno = 0;
-  char *endptr;
-  int left = strtol(left_str, &endptr, 10);
-  if (left_str == endptr) {
-    errno = EINVAL;
-  }
-  if (errno != 0) {
+  int left;
+  if (to_int(left_str, &left) != 0) {
     perror(argv[0]);
     return EXIT_FAILURE;
   }
@@ -41,12 +50,8 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Op must be one character. Got `%s'.\n", op_str);
     return EXIT_FAILURE;
   }
-  errno = 0;
-  int right = strtol(right_str, &endptr, 10);
-  if (right_str == endptr) {
-    errno = EINVAL;
-  }
-  if (errno != 0) {
+  int right;
+  if (to_int(right_str, &right) != 0) {
     perror(argv[0]);
     return EXIT_FAILURE;
   }
