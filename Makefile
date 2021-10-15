702f248cfa341773595d32af84c3a65d5ec3e156
Stuart S. Scolastico <stuart.scolastico@tufts.edu>
2021-10-15 11:48:35 -0400

    Add test suite

--- Makefile
+++ Makefile
@@ -3,6 +3,10 @@ all: main
 main: main.c
 	gcc main.c -o main
 
+.PHONY: test
+test: main
+	./main-tests.sh
+
 .PHONY: clean
 clean:
 	rm -f main
---
+++ main-tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+die() {
+  echo "Error: $1" >&2
+  exit 1
+}
+
+expect_eq() {
+  RESULT="$(./main "$2" "$3" "$4")"
+  [ "$RESULT" = "$1" ] || die "Expected '$2 $3 $4' == $1 but got $RESULT"
+}
+
+expect_error() {
+  ./main "$1" "$2" "$3" > /dev/null && die "Expected '$1 $2 $3' to be an error but it wasn't"
+}
+
+expect_eq 3 1 + 2
+expect_eq -1 3 - 4
+expect_eq 10 2 '*' 5
+expect_eq 20 100 / 5
+
+expect_error 1 + qwerty
+expect_error qwerty + 2
